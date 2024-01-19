689502cb1d0c915f4b258f14dfebfee259bc1f24
x97gfq <jamie.symonds@gmail.com>
2024-01-19 13:36:57 -0400

    updated acitivty2

--- Week_03_JavaScript_Examples/acitiity2/script.js
+++ Week_03_JavaScript_Examples/acitiity2/script.js
@@ -1,2 +1,21 @@
 
+function buttonClicked() {
+    var username = document.getElementById("username");
+    if (username) {
+        var nm = username.value;
+        alert("welcome, " + nm);
+        nm.value = "";
+    }
+};
+
+window.addEventListener("load", 
+    function(event) {
+        var elem = document.getElementById("user_button");
+        if (elem){
+            elem.addEventListener("click",buttonClicked); 
+        } 
+    }
+);
+
+
 
