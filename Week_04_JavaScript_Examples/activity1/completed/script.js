feb915eba160699e79ab2dbc9cb7179b4975a865
Jamie Symonds <jamie.symonds@gmail.com>
2024-01-26 12:39:59 -0400

    update for Friday activity

--- Week_04_JavaScript_Examples/activity1/completed/script.js
+++ Week_04_JavaScript_Examples/activity1/completed/script.js
@@ -15,6 +15,31 @@ function checkForMatch(array, number) {
     return array.includes(number);
 }
 
+function getDiceImageName(number) {
+    var img = "";
+    switch(number) {
+        case 1:
+            img = "one.JPG";
+            break;
+        case 2:
+            img = "two.JPG";
+            break;
+        case 3:
+            img = "three.JPG";
+            break;
+        case 4:
+            img = "four.JPG";
+            break;
+        case 5:
+            img = "five.JPG";
+            break;
+        case 6:
+            img = "six.JPG";
+            break;
+    }
+    return "../images/" + img;
+}
+
 function playGame() {
     // Settings
     const numCount = 10; // Number of random numbers in the array
@@ -31,9 +56,28 @@ function playGame() {
     const isMatch = checkForMatch(randomNumbers, rolledNumber);
     console.log(isMatch ? "Match found!" : "No match.");
 
-    var resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = "Numbers: " + randomNumbers.toString() + ", rolled: " + rolledNumber + " " + (isMatch ? "Match found!" : "No match.");
+    //var resultsDiv = document.getElementById("results");
+    //resultsDiv.innerHTML = "<p>Numbers: " + randomNumbers.toString() + ", rolled: " + rolledNumber + " " + (isMatch ? "Match found!" : "No match.") + "</p>";
+    
+    // show dice images
+    document.getElementById("diceRoll").src = getDiceImageName(rolledNumber);
+
+    document.getElementById("dice1").src = getDiceImageName(randomNumbers[0]);
+    document.getElementById("dice2").src = getDiceImageName(randomNumbers[1]);
+    document.getElementById("dice3").src = getDiceImageName(randomNumbers[2]);
+    document.getElementById("dice4").src = getDiceImageName(randomNumbers[3]);
+    document.getElementById("dice5").src = getDiceImageName(randomNumbers[4]);
+    document.getElementById("dice6").src = getDiceImageName(randomNumbers[5]);
+    document.getElementById("dice7").src = getDiceImageName(randomNumbers[6]);
+    document.getElementById("dice8").src = getDiceImageName(randomNumbers[7]);
+    document.getElementById("dice9").src = getDiceImageName(randomNumbers[8]);
+    document.getElementById("dice10").src = getDiceImageName(randomNumbers[9]);
+
+    document.getElementById("winner").style.display = (isMatch ? "inline" : "none");
+    document.getElementById("loser").style.display = (isMatch ? "none" : "inline");
     //
+    //next step - ?
+    //How many exact matches with roll?
     //
 }
 
