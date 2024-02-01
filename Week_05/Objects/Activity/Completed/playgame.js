a005323aaf5b8e6d61e76ef54837ba560a3ad2b8
Jamie Symonds <jamie.symonds@gmail.com>
2024-02-01 08:21:20 -0400

    updated activity

--- Week_05/Objects/Activity/Completed/playgame.js
+++ Week_05/Objects/Activity/Completed/playgame.js
@@ -1,14 +1,23 @@
-let player = new Player("Hero");
-let enemy = new Enemy("Goblin");
 
-console.log("A wild enemy appears!");
+function playGame() {
 
-// Player attacks
-player.attack(enemy);
+    let player = new Player("Hero");
+    let enemy = new Enemy("Goblin");
+    
+    console.log("A wild enemy appears!");
+    
+    // Player attacks
+    player.attack(enemy);
+    
+    // If you want to allow the enemy to counterattack
+    // enemy.attack(player);
+    
+    // You can add more interactions, like checking if the enemy is defeated, 
+    // adding more enemies, or creating a loop for multiple turns.
+    
+}
 
-// If you want to allow the enemy to counterattack
-// enemy.attack(player);
-
-// You can add more interactions, like checking if the enemy is defeated, 
-// adding more enemies, or creating a loop for multiple turns.
-
+window.addEventListener("load", (event) => {
+    const btn = document.getElementById("click_btn");
+    btn.addEventListener("click", playGame);
+});
