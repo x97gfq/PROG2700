8c8a7604bab7266d82c93bd2ce4f9af65a076802
Jamie Symonds <jamie.symonds@gmail.com>
2024-02-11 16:00:02 -0400

    week 7

---
+++ Week_07_jQuery/ajax_example/script.js
@@ -0,0 +1,43 @@
+
+function getTriviaQuestion() {
+    // Construct the URL 
+    var url = "https://opentdb.com/api.php?amount=1&category=18&difficulty=medium&type=multiple";
+
+    // Make an AJAX request to the API
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            // Handle the response here
+            console.log(data);
+
+            //alert(data.results[0].question);
+            //
+            //
+            // and display the data
+            //
+            //
+        },
+        error: function(error) {
+            // Handle errors here
+            console.error("Error fetching data:", error);
+        }
+    });
+}
+
+function evaluateTriviaQuestion() {
+    alert("checking...");
+    //correct? 
+    //
+    //
+    //get next question
+    this.getTriviaQuestion();
+}
+
+window.addEventListener("load", (event) => {
+    this.getTriviaQuestion();
+
+    const btn = document.getElementById("submitAnswer");
+    btn.addEventListener("click", evaluateTriviaQuestion);
+});
